dc4b2e6d38033c6427a0b162487ecaa1f4129839
Dolu <doludev@protonmail.com>
2022-05-26 13:56:44 +0200

    Add css on the public login component

--- resources/js/zerologin.js
+++ resources/js/zerologin.js
@@ -7,15 +7,17 @@ const MyVueElement = defineCustomElement({
   data() {
     return {
       lnurl: '',
+      textCopy: 'Click to copy'
     }
   },
   template: `
   <div class="zl">
     <canvas class="zl-canvas" ref="canvas"></canvas>
-    <div class="zl-lnurl-input-group">
+    <div class="zl-lnurl-input-group" @click="copy()">
       <input type="text" v-model="lnurl" disabled="true" />
-      <button type="button" @click="copy()">Copy</button>
+      <span class="zl-copy-text" ref="copyText">{{textCopy}}</span>
     </div>
+    <div class="zl-powered">Powered by <a href="https://zerologin.co" target="_blank">Zerologin</a></div>
   </div>
   `,
   styles: [`
@@ -24,10 +26,47 @@ const MyVueElement = defineCustomElement({
     flex-direction: column;
     align-items: center;
   }
+  
+  .zl-lnurl-input-group {
+    position: relative;
+    cursor: pointer;
+  }
+  .zl-lnurl-input-group > input {
+    width: 180px;
+    padding: 5px 10px;
+    background-color: #fff;
+    border: 1px solid #000;
+    border-radius: 0;
+    cursor: pointer;
+  }
+  .zl-lnurl-input-group > .zl-copy-text{
+    position: absolute;
+    top: 15%;
+    left: 30%;
+    display: none;
+    cursor: pointer;
+  }
+  .zl-lnurl-input-group:hover > input{
+    filter: blur(5px)
+  }
+  .zl-lnurl-input-group:hover > input + .zl-copy-text{
+    display: block;
+  }
+
+  .zl-powered{
+    margin-top:10px;
+    font-size: 12px;
+    color: #626262;
+  }
+  .zl-powered > a{
+    color: #000;
+  }
   `],
   methods: {
     copy() {
+      this.textCopy = 'Copied'
       navigator.clipboard.writeText(this.lnurl);
+      setTimeout(() => { this.textCopy = 'Click to copy' }, 3000)
     }
   },
   mounted() {
