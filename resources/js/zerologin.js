2590ce2558242d407dba0f473b0f0fb27290dd5e
Dolu <doludev@protonmail.com>
2022-06-13 18:47:00 +0200

    Add webln login option when available

--- resources/js/zerologin.js
+++ resources/js/zerologin.js
@@ -1,17 +1,27 @@
 import QRCode from 'qrcode'
 import { defineCustomElement } from 'vue'
 
-const MyVueElement = defineCustomElement({
+const ZeroLoginElement = defineCustomElement({
   props: { zlurl: String },
   emits: {},
   data() {
     return {
       lnurl: '',
       textCopy: 'Click to copy',
+
+      weblnSupported: false,
     }
   },
   template: `
   <div class="zl">
+    <div class="zl-webln" v-if="weblnSupported">
+        <div>Login browser extension detected</div>
+        <button @click="weblnConnect()">Open</button>
+
+        <div class="or-scan">or scan</div>
+    </div>
+
+
     <canvas class="zl-canvas" ref="canvas"></canvas>
     <div class="zl-lnurl-input-group" @click="copy()">
       <input type="text" v-model="lnurl" disabled="true" />
@@ -20,6 +30,60 @@ const MyVueElement = defineCustomElement({
     <div class="zl-powered">Powered by <a href="https://zerologin.co" target="_blank">Zerologin</a></div>
   </div>
   `,
+  methods: {
+    copy() {
+      this.textCopy = 'Copied'
+      navigator.clipboard.writeText(this.lnurl)
+      setTimeout(() => {
+        this.textCopy = 'Click to copy'
+      }, 3000)
+    },
+    async weblnConnect() {
+      await window.webln.enable()
+      try {
+        await webln.lnurl(this.lnurl)
+      }
+      catch (e) {
+        console.error(e);
+      }
+    }
+  },
+  async mounted() {
+    const source = new EventSource(this.zlurl + '/sse/lnurl')
+    source.addEventListener(
+      'message',
+      (e) => {
+        const parsed = JSON.parse(e.data)
+        if (parsed.message === 'challenge') {
+          this.lnurl = parsed.encoded
+          QRCode.toCanvas(this.$refs.canvas, parsed.encoded, function (error) {
+            if (error) console.error(error)
+          })
+
+
+        }
+        if (parsed.message === 'loggedin') {
+          window.location = parsed.callback
+        }
+      },
+      false
+    )
+
+    const maxInterval = 1500
+    let currentIntervalChecking = 0;
+    const intervalTime = 100
+    const interval = setInterval(() => {
+      currentIntervalChecking += intervalTime
+      if (typeof window.webln !== 'undefined') {
+        this.weblnSupported = true
+        clearInterval(interval)
+      }
+      if (currentIntervalChecking >= maxInterval) {
+        clearInterval(interval)
+      }
+    }, intervalTime)
+
+  },
   styles: [
     `
   .zl{
@@ -27,7 +91,30 @@ const MyVueElement = defineCustomElement({
     flex-direction: column;
     align-items: center;
   }
-  
+
+  .zl-webln {
+    font-size: 12px;
+    color: #626262;
+    text-align: center;
+  }
+  .zl-webln > button {
+    background-color: #000;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    padding: 8px 15px;
+    cursor: pointer;
+    margin-top: 3px;
+  }
+  .zl-webln > button:hover {
+    background-color: #4d4d4d;
+  }
+
+  .or-scan {
+    margin-top: 15px;
+    margin-bottom: 5px;
+  }
+
   .zl-lnurl-input-group {
     position: relative;
     cursor: pointer;
@@ -63,56 +150,7 @@ const MyVueElement = defineCustomElement({
     color: #000;
   }
   `,
-  ],
-  methods: {
-    copy() {
-      this.textCopy = 'Copied'
-      navigator.clipboard.writeText(this.lnurl)
-      setTimeout(() => {
-        this.textCopy = 'Click to copy'
-      }, 3000)
-    },
-  },
-  mounted() {
-    const source = new EventSource(this.zlurl + '/sse/lnurl')
-    source.addEventListener(
-      'message',
-      (e) => {
-        const parsed = JSON.parse(e.data)
-        if (parsed.message === 'challenge') {
-          this.lnurl = parsed.encoded
-          QRCode.toCanvas(this.$refs.canvas, parsed.encoded, function (error) {
-            if (error) console.error(error)
-          })
-        }
-        if (parsed.message === 'loggedin') {
-          window.location = parsed.callback
-        }
-      },
-      false
-    )
-
-    source.addEventListener(
-      'open',
-      function (e) {
-        console.log(e)
-      },
-      false
-    )
-
-    // source.addEventListener(
-    //   'error',
-    //   function (e) {
-    //     if (e.eventPhase == EventSource.CLOSED) source.close()
-    //     if (e.target.readyState == EventSource.CLOSED) {
-    //       console.log('Disconnected')
-    //     } else if (e.target.readyState == EventSource.CONNECTING) {
-    //       console.log('Connecting...')
-    //     }
-    //   },
-    //   false
-    // )
-  },
+  ]
 })
 
-customElements.define('zero-login', MyVueElement)
+customElements.define('zero-login', ZeroLoginElement)
