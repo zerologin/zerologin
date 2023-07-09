9857e244317ce95e3ce128dbd61c9520b5b5ead1
Dolu <doludev@protonmail.com>
2023-07-09 18:06:43 +0200

    fix auth middleware for sigauth

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -5,6 +5,7 @@ import Utils from 'App/Utils'
 import Env from '@ioc:Adonis/Core/Env'
 import Domain from 'App/Models/Domain'
 import Encryption from '@ioc:Adonis/Core/Encryption'
+import SigauthDomain from 'App/Models/SigauthDomain'
 
 export default class Jwt {
   public async handle(ctx: HttpContextContract, next: () => Promise<void>) {
@@ -16,24 +17,33 @@ export default class Jwt {
           return ctx.response.unauthorized("You don't have the permission to access this page")
         }
 
-        const externalDomain = await Domain.query().where('public_id', publicIdCookie).first()
-        if (!externalDomain) {
+        let tokenName: string | null = null
+        let jwtSecret: string | null = null
+        const lnurlDomain = await Domain.query().where('public_id', publicIdCookie).first()
+        const sigauthDomain = await SigauthDomain.query().where('id', publicIdCookie).first()
+        if (lnurlDomain) {
+          tokenName = lnurlDomain.tokenName
+          jwtSecret = lnurlDomain.jwtSecret
+        }
+        else if (sigauthDomain) {
+          tokenName = sigauthDomain.tokenName
+          jwtSecret = sigauthDomain.jwtSecret
+        }
+        else {
           return ctx.response.unauthorized("Cannot find the domain configuration associated with this request")
         }
 
-        const jwtCookie = JwtService.getFromCookie(ctx, externalDomain.tokenName)
+        const jwtCookie = JwtService.getFromCookie(ctx, tokenName)
         if (!jwtCookie) {
           return ctx.response.unauthorized("You don't have the permission to access this page")
         }
 
-        if (externalDomain) {
-          const decryptedJwtSecret: string | null = Encryption.decrypt(externalDomain.jwtSecret)
-          if (!decryptedJwtSecret) {
-            return ctx.response.unprocessableEntity()
-          }
-          const payload = await JwtService.verify(jwtCookie, decryptedJwtSecret)
-          ctx.request.user = await UserService.getOrCreate(payload.pubKey)
+        const decryptedJwtSecret: string | null = Encryption.decrypt(jwtSecret)
+        if (!decryptedJwtSecret) {
+          return ctx.response.unprocessableEntity()
         }
+        const payload = await JwtService.verify(jwtCookie, decryptedJwtSecret)
+        ctx.request.user = await UserService.getOrCreate(payload.pubKey)
       } else {
         const jwtCookie = JwtService.getFromCookie(ctx)
         if (!jwtCookie) {
